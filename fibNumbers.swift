6e406cc0f56f3bba5b1561e5a68e6bc206a895e9
Enes Nurullah Kendirci <enesnkendirci@gmail.com>
2021-09-10 22:08:10 +0300

    Comment lines added.

--- fibNumbers.swift
+++ fibNumbers.swift
@@ -2,14 +2,14 @@ calculateFib(firstNumber: 0, secondNumber: 1)
 
 func calculateFib(firstNumber x: Int, secondNumber y: Int) {
     
-    let z = x + y
+    let z = x + y //new Fibonacci number
     
-    if z > 1000 {
+    if z > 1000 { //condition that ends the recursion.
         return
     }
-    if z > 750 {
+    if z > 750 {//print condition
         print(z)
     }
     
-    calculateFib(firstNumber: y, secondNumber: z)
+    calculateFib(firstNumber: y, secondNumber: z)//recursive
 }
